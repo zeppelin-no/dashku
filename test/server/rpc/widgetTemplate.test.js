6bc12496c5bd6d88882617e81134ed32eff3dfa1
Paul Jensen <paulbjensen@gmail.com>
2014-02-28 18:16:43 +0000

    Converted WidgetTemplate Test file from Coffee to JS

---
+++ test/server/rpc/widgetTemplate.test.js
@@ -0,0 +1,46 @@
+'use strict';
+
+
+
+// Dependencies
+//
+var assert          = require('assert');
+var ss              = require('socketstream');
+require('../../../server/internals');
+var WidgetTemplate  = ss.api.app.models.WidgetTemplate;
+var ass             = ss.start();
+
+describe('WidgetTemplate', function () {
+
+
+
+	describe('getAll', function () {
+
+
+
+		describe('if successful', function () {
+
+
+
+			it('should return a success status with a list of widget templates', function (done) {
+				new WidgetTemplate({name: 'Widge'}).save(function (err) {
+					assert.equal(null, err);
+					ass.rpc('widgetTemplate.getAll', function (res) {
+						assert.equal(res[0].status, 'success');
+						assert.deepEqual(res[0].widgetTemplates[0].name, 'Widge');
+						done();
+					});
+				});
+			});
+
+
+
+		});
+
+
+
+	});
+
+
+
+});
